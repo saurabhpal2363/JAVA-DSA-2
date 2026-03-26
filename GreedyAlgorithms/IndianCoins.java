88117e47b2c3fd2c0aeddff96e0a6ba564e96be8
saurabhpal2363 <saurabhpal2363@gmail.com>
2026-03-26 21:46:28 +0530

    Problem on Indian Coins

---
+++ GreedyAlgorithms/IndianCoins.java
@@ -0,0 +1,27 @@
+package GreedyAlgorithms;
+
+import java.util.*;
+public class IndianCoins {
+    public static void main(String[] args) {
+        Integer coins[] = {1, 2, 5, 10, 20, 50, 100, 500, 2000};
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        Arrays.sort(coins, Comparator.reverseOrder());
+
+        int countOfCoins = 0;
+        int amount = 590;
+
+        for(int i=0; i<coins.length; i++) {
+            if(coins[i] <= amount) {
+                while(coins[i] <= amount) {
+                    countOfCoins++;
+                    ans.add(coins[i]);
+                    amount -= coins[i];
+                }
+            }
+        }
+
+        System.out.println(ans);
+        System.out.println(countOfCoins);
+    }
+}
