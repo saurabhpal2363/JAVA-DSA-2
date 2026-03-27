93c8e1b3ae30e8a5e79ca2551dd6cc2c4fbee8b1
saurabhpal2363 <saurabhpal2363@gmail.com>
2026-03-27 21:52:44 +0530

    Analyze their time complexity

--- GreedyAlgorithms/IndianCoins.java
+++ GreedyAlgorithms/IndianCoins.java
@@ -1,7 +1,7 @@
 package GreedyAlgorithms;
 
 import java.util.*;
-public class IndianCoins {
+public class IndianCoins {      //  O(n)
     public static void main(String[] args) {
         Integer coins[] = {1, 2, 5, 10, 20, 50, 100, 500, 2000};
         ArrayList<Integer> ans = new ArrayList<>();
