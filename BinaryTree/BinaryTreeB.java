4e428f9bfb691190d417c7c0814531411a163482
saurabhpal2363 <saurabhpal2363@gmail.com>
2026-04-04 20:03:29 +0530

    Binary Tree traversal

--- BinaryTree/BinaryTreeB.java
+++ BinaryTree/BinaryTreeB.java
@@ -1,5 +1,7 @@
 package BinaryTree;
 
+import java.util.*;
+
 // Build Tree --> O(n)
 public class BinaryTreeB {
     static class Node {
@@ -28,11 +30,75 @@ public class BinaryTreeB {
 
             return newNode;
         }
+
+        public static void preorder(Node root) {    // O(n)
+            if(root == null) {
+                return;
+            }
+
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        public static void inorder(Node root) {     // O(n)
+            if(root == null) {
+                return;
+            }
+
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+        public static void postorder(Node root) {   // O(n)
+            if(root == null) {
+                return;
+            }
+
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+
+        public static void levelOrder(Node root) {  // O(n)
+            if(root == null) {
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()) {
+                Node currNode = q.remove();
+                if(currNode == null) {
+                    System.out.println();
+                    if(q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(currNode.data + " ");
+                    if(currNode.left != null) {
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right != null) {
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
+        // tree.preorder(root);
+        // tree.inorder(root);
+        // tree.postorder(root);
+        tree.levelOrder(root);
     }
 }
---
+++ BinaryTree/Questions.java
@@ -0,0 +1,62 @@
+package BinaryTree;
+
+public class Questions {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int height(Node root) {   // O(n)
+        if(root == null) {
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        return Math.max(lh,rh) + 1;
+    }
+
+    public static int count(Node root) {    // O(n)
+        if(root == null) {
+            return 0;
+        }
+
+        int lCount = count(root.left);
+        int rCount = count(root.right);
+
+        return lCount + rCount + 1;
+    }
+
+    public static int sum(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int lSum = sum(root.left);
+        int rSum = sum(root.right);
+
+        return lSum + rSum + root.data;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(height(root));
+        System.out.println(count(root));
+        System.out.println(sum(root));
+    }
+}
