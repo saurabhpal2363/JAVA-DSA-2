3ca10cbf5d0e0611515968606243a68fb577f0c1
saurabhpal2363 <saurabhpal2363@gmail.com>
2026-04-05 23:00:59 +0530

    Problems on Binary Tree

--- BinaryTree/Questions.java
+++ BinaryTree/Questions.java
@@ -35,7 +35,7 @@ public class Questions {
         return lCount + rCount + 1;
     }
 
-    public static int sum(Node root) {
+    public static int sum(Node root) {  // O(n)
         if(root == null) {
             return 0;
         }
@@ -44,6 +44,45 @@ public class Questions {
         int rSum = sum(root.right);
 
         return lSum + rSum + root.data;
+    }
+
+    public static int diameter2(Node root) { // O(n^2)
+        if(root == null) {
+            return 0;
+        }
+
+        int leftDiam = diameter2(root.left);
+        int leftHt = height(root.left);
+        int rightDiam = diameter2(root.right);
+        int rightHt = height(root.right);
+
+        int selfDiam = leftHt + rightHt + 1;
+
+        return Math.max(selfDiam, Math.max(leftDiam, rightDiam));
+    }
+
+    static class Info {
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht) {
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    public static Info diameter(Node root) {    // O(n)
+        if(root == null) {
+            return new Info(0, 0);
+        }
+
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+        return new Info(diam, ht);
     }
 
     public static void main(String[] args) {
@@ -55,8 +94,9 @@ public class Questions {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        System.out.println(height(root));
-        System.out.println(count(root));
-        System.out.println(sum(root));
+        // System.out.println(height(root));
+        // System.out.println(count(root));
+        // System.out.println(sum(root));
+        System.out.println(diameter(root).diam);
     }
 }
