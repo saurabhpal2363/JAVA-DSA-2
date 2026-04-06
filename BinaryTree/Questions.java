e0628103500129ba6eba414f7c7e75431745e938
saurabhpal2363 <saurabhpal2363@gmail.com>
2026-04-06 21:05:18 +0530

    Lowest Common Ancestors

--- BinaryTree/Questions.java
+++ BinaryTree/Questions.java
@@ -1,5 +1,7 @@
 package BinaryTree;
 
+import java.util.*;
+
 public class Questions {
     static class Node {
         int data;
@@ -85,6 +87,82 @@ public class Questions {
         return new Info(diam, ht);
     }
 
+    public static void KLevel(Node root, int level, int k) {    // O(n)
+        if(root == null) {
+            return;
+        }
+
+        if(level == k) {
+            System.out.print(root.data + " ");
+            return;
+        }
+
+        KLevel(root.left, level+1, k);
+        KLevel(root.right, level+1, k);
+    }
+
+    // Ques. Lowest Common Ancestors(Approach - 1)    -->  O(n)
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if(root == null) {
+            return false;
+        }
+
+        path.add(root);
+
+        if(root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if(foundLeft || foundRight) {
+            return true;
+        }
+
+        path.remove(path.size()-1);
+
+        return false;
+    }
+
+    public static Node lowestCommonAncestor(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        int i = 0;
+        for(; i<path1.size() && i<path2.size(); i++) {
+            if(path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+
+        Node lowestCommonAncestor = path1.get(i-1);
+
+        return lowestCommonAncestor;
+    } 
+
+    // Ques. Lowest Common Ancestors(Approach - 2)    -->  O(n)
+    public static Node lowestCommonAncestor2(Node root, int n1, int n2) {
+        if(root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lowestCommonAncestor2(root.left, n1, n2);
+        Node rightLca = lowestCommonAncestor2(root.right, n1, n2);
+
+        if(rightLca == null) {
+            return leftLca;
+        }
+        if(leftLca == null) {
+            return rightLca;
+        }
+
+        return root;
+    }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -97,6 +175,12 @@ public class Questions {
         // System.out.println(height(root));
         // System.out.println(count(root));
         // System.out.println(sum(root));
-        System.out.println(diameter(root).diam);
+        // System.out.println(diameter(root).diam);
+
+        // int k = 2;
+        // KLevel(root, 1, k);
+
+        int n1 = 4, n2 = 5;
+        System.out.println(lowestCommonAncestor2(root, n1, n2).data);
     }
 }
