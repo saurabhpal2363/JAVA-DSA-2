44dc06f2968e59badbb14e926ec0db4e36deedc6
saurabhpal2363 <saurabhpal2363@gmail.com>
2026-04-08 20:10:40 +0530

    Problems solving on Binary Tree

--- BinaryTree/Questions.java
+++ BinaryTree/Questions.java
@@ -144,7 +144,7 @@ public class Questions {
         return lowestCommonAncestor;
     } 
 
-    // Ques. Lowest Common Ancestors(Approach - 2)    -->  O(n)  && No Space complexity
+    // Ques. Lowest Common Ancestors(Approach - 2)    -->  O(n)
     public static Node lowestCommonAncestor2(Node root, int n1, int n2) {
         if(root == null || root.data == n1 || root.data == n2) {
             return root;
@@ -161,6 +161,92 @@ public class Questions {
         }
 
         return root;
+    }
+
+    // Ques. Minimum Distance between Nodes 
+    public static int lcaDist(Node root, int n) {
+        if(root == null) {
+            return -1;
+        }
+
+        if(root.data == n) {
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if(leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lowestCommonAncestor2(root, n1, n2);
+        
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
+    }
+
+    // Ques. Kth Ancestor of Node 
+    public static int KAncestor(Node root, int n, int k) {
+        if(root == null) {
+            return -1;
+        }
+
+        if(root.data == n) {
+            return 0;
+        }
+
+        int leftDist = KAncestor(root.left, n, k);
+        int rightDist = KAncestor(root.right, n, k);
+
+        if(leftDist == -1 && rightDist == -1) {
+            return -1;
+        }
+
+        int max = Math.max(leftDist, rightDist);
+
+        if(max+1 == k) {
+            System.out.println(root.data);
+        }
+
+        return max + 1;
+    }
+
+    // Ques. Transform to sum tree 
+    public static int transform(Node root) {    // O(n)
+        if(root == null) {
+            return 0;
+        }
+
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+
+        root.data = newLeft + leftChild + newRight + rightChild;
+
+        return data;
+    }
+
+    public static void preorder(Node root) {
+        if(root == null) {
+            return;
+        }
+
+        System.out.print(root.data +" ");
+        preorder(root.left);
+        preorder(root.right);
     }
 
     public static void main(String[] args) {
@@ -180,7 +266,16 @@ public class Questions {
         // int k = 2;
         // KLevel(root, 1, k);
 
-        int n1 = 4, n2 = 5;
-        System.out.println(lowestCommonAncestor2(root, n1, n2).data);
+        // int n1 = 4, n2 = 5;
+        // System.out.println(lowestCommonAncestor2(root, n1, n2).data);
+
+        // int n1 = 4, n2 = 5;
+        // System.out.println(minDist(root, n1, n2));
+
+        // int n = 5, k = 1;
+        // KAncestor(root, n, k);
+
+        transform(root);
+        preorder(root);
     }
 }
