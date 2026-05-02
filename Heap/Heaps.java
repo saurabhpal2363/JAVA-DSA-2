46b2e1f7a89167bedf43ae5a33f4c2c6bc74b295
saurabhpal2363 <saurabhpal2363@gmail.com>
2026-05-02 18:06:36 +0530

    Questions on Heap

--- Heap/Heaps.java
+++ Heap/Heaps.java
@@ -16,12 +16,38 @@ public class Heaps {
             while(arr.get(x) < arr.get(par)) {      // O(logn)
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
-                arr.set(temp, arr.get(x));
+                arr.set(par, temp);
+
+                x = par;
+                par = (x-1) / 2;
             }
         }
 
         public int peek() {
             return arr.get(0);
+        }
+
+        private void heapify(int i) {       // O(logn)
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int minIdx = i;
+
+            if(left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+
+            if(right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if(minIdx != i) {
+                // Swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
         }
 
         public int remove() {
@@ -36,11 +62,26 @@ public class Heaps {
             arr.remove(arr.size() - 1);
 
             // Step3. Heapify
-            
+            heapify(0);
+
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
         }
     }
 
     public static void main(String[] args) {
-        
+        Heap pq = new Heap();
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(5);
+
+        while(!pq.isEmpty()) {      // Heap sort  - O(nlogn)
+            System.out.println(pq.peek());
+            pq.remove();
+        }
     }
 }
