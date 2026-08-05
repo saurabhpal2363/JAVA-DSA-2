3b173ecd48f0bfe0eb534a04d87d6585a6096218
saurabhpal2363 <saurabhpal2363@gmail.com>
2026-08-05 16:25:57 +0530

    LabPractice on Scanner Class and Class Object

--- Practice.java
+++ Practice.java
@@ -1,46 +1,15 @@
-class Practice {
-    static class Node {
-        int data;
-        Node left;
-        Node right;
-
-        Node(int data) {
-            this.data = data;
-        }
+class Square {
+    int length;
+    void area() {
+        int area = length*length;
+        System.out.println(area);
     }
+}
 
-    public static Node insert(Node root, int val) {
-        if(root == null) {
-            root = new Node(val);
-            return root;
-        }
-
-        if(root.data > val) {
-            root.left = insert(root.left, val);
-        } else {
-            root.right = insert(root.right, val);
-        }
-
-        return root;
-    }
-
-    public static void inorder(Node root) {
-        if(root == null) {
-            return;
-        }
-
-        inorder(root.left);
-        System.out.print(root.data + " ");
-        inorder(root.right);
-    }
-    public static void main(String args[]){
-        int arr[] = {6, 9, 3, 4, 5, 7, 2, 1, 10, 8};
-        Node root = null;
-
-        for(int i=0; i<arr.length; i++) {
-            root = insert(root, arr[i]);
-        }
-
-        inorder(root);
+public class Practice {
+    public static void main(String[] args) {
+        Square sq = new Square();
+        sq.length = 5;
+        sq.area();
     }
 }
