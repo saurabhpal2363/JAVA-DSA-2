a0755d49ee0797dfcd9371a469ce256944d244a1
saurabhpal2363 <saurabhpal2363@gmail.com>
2026-08-12 16:28:10 +0530

    Grading System

--- Practice.java
+++ Practice.java
@@ -1,15 +1,23 @@
-class Square {
-    int length;
-    void area() {
-        int area = length*length;
-        System.out.println(area);
-    }
-}
+import java.util.Scanner;
 
 public class Practice {
+    public static String grade(int marks) {
+        if(marks>=0 && marks<=34)
+            return "fail";
+        else if(marks>=35 && marks<=49)
+            return "third class";
+        else if(marks>=50 && marks<=59)
+            return "second class";
+        else if(marks>=60 && marks<=74)
+            return "first class";
+        else if(marks>=75 && marks<=100)
+            return "distinction";
+        else
+            return "INVALID";
+    }
     public static void main(String[] args) {
-        Square sq = new Square();
-        sq.length = 5;
-        sq.area();
+        System.out.println(grade(54));
+        System.out.println(grade(91));
+        System.out.println(grade(154));
     }
 }
