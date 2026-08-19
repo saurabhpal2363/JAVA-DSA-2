722101b1d5596539319a340a3a33e708a77e242e
saurabhpal2363 <saurabhpal2363@gmail.com>
2026-08-19 16:25:55 +0530

    Git manipulation

--- Practice.java
+++ Practice.java
@@ -1,23 +1,18 @@
-import java.util.Scanner;
-
 public class Practice {
-    public static String grade(int marks) {
-        if(marks>=0 && marks<=34)
-            return "fail";
-        else if(marks>=35 && marks<=49)
-            return "third class";
-        else if(marks>=50 && marks<=59)
-            return "second class";
-        else if(marks>=60 && marks<=74)
-            return "first class";
-        else if(marks>=75 && marks<=100)
-            return "distinction";
-        else
-            return "INVALID";
-    }
     public static void main(String[] args) {
-        System.out.println(grade(54));
-        System.out.println(grade(91));
-        System.out.println(grade(154));
+        int n = 5;
+        int f1 = 0;
+        int f2 = 1;
+        int f = f1 + f2;
+
+        System.out.print(f1 + " ");
+        System.out.print(f2 + " ");
+
+        while(f<=n) {
+            System.out.print(f + " ");
+            f1 = f2;
+            f2 = f;
+            f = f1 + f2;
+        }
     }
 }
